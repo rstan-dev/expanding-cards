1b64de924f67b424d6909902b4674dccd9cd7c37
Russ-S <rstanleym74@gmail.com>
2023-02-01 16:24:50 +0000

    walkthrough project on creating expanding photo cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,18 @@
-let x = "test2";
+const panels = document.querySelectorAll('.panel');
+// selects all elements with the class of "panel"
+console.log(panels); //shows the node list in the console
+
+
+panels.forEach((panel) => {
+    panel.addEventListener('click', () => {
+        console.log(panel); //shows the detail of each panel
+        removeActiveClasses(); //func to remove class active
+        panel.classList.add('active');
+    });
+});
+
+function removeActiveClasses() {
+    panels.forEach(panel => {
+        panel.classList.remove('active');
+    });
+};
